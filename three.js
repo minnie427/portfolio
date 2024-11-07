770def42d898d8d6b414064757492dcfe386f440
Minnie Park <minniepark.studio@gmail.com>
2024-11-07 23:20:36 +1100

    fix issues

--- three.js
+++ three.js
@@ -11,8 +11,9 @@ let box;
 let duck;
 let controls;
 let intersected;
-
-
+let isDragging = false;
+let mouseDownTime = 0;
+const CLICK_TIME_THRESHOLD = 200; // milliseconds
 
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
@@ -25,12 +26,6 @@ const models = [
     scale: 0.3,
     rotate: 0.75,
   },
- /*  {
-    gltf: "./data/rose1.glb",
-    link: "./pages/spring.html",
-    position: [0, -0.5, 0],
-    scale: 25,
-  }, */
   {
     gltf: "./data/rose1.glb",
     link: "./pages/voice.html",
@@ -63,7 +58,7 @@ const models = [
   },
   {
     gltf: "./data/cupcake4.glb",
-    link: "./pages/cupcake.html",
+    link: "./cupcake",
     position: [0, -0.4, 2],
     scale: 2.1,
   },
@@ -79,13 +74,36 @@ const models = [
     position: [-1.8, -0.3, 2.2],
     scale: 0.26,
   }
+];
 
-]
+function onMouseDown(event) {
+  mouseDownTime = Date.now();
+  isDragging = false;
+}
 
+function onMouseMove(event) {
+  mouse.x = (event.clientX / container.clientWidth) * 2 - 1;
+  mouse.y = -(event.clientY / container.clientHeight) * 2 + 1;
+  
+  if (mouseDownTime > 0) {
+    isDragging = true;
+  }
+}
 
-function onMouseMove( event ) {
-	mouse.x = ( event.clientX / container.clientWidth ) * 2 - 1;
-	mouse.y = - ( event.clientY / container.clientHeight ) * 2 + 1;
+function onMouseUp(event) {
+  const clickDuration = Date.now() - mouseDownTime;
+  
+  if (!isDragging && clickDuration < CLICK_TIME_THRESHOLD) {
+    raycaster.setFromCamera(mouse, camera);
+    const intersects = raycaster.intersectObjects(modelContainer.children);
+    if (intersects.length > 0) {
+      const { link } = intersects[0].object.userData;
+      location.replace(link, '_blank');
+    }
+  }
+  
+  mouseDownTime = 0;
+  isDragging = false;
 }
 
 container = document.querySelector('.scene');
@@ -113,35 +131,20 @@ const light = new THREE.DirectionalLight(0xffffff, 0.5);
 light.position.set(0,1,0);
 scene.add(light);
 
-/* const dl = new THREE.DirectionalLight(0xffffff, 0.5);
-dl.position.set(-2, 2, 0);
-const dlHelper = new THREE.DirectionalLight(dl, 3);
-scene.add(dl, dlHelper); */
-
 const sl = new THREE.SpotLight(0xffc0fc0, 500, 10, 0.2, 0.5);
 sl.position.set(5,3,0);
-/* const slHelper = new THREE.SpotLightHelper(sl); */
 scene.add(sl);
 
 //renderer
-
 renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.outputColorSpace = THREE.SRGBColorSpace;
-/* renderer.setClearColor(0x000000); */
 renderer.setSize(container.clientWidth, container.clientHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 container.appendChild(renderer.domElement);
 
-controls = new OrbitControls (camera, renderer.domElement);
-controls.enableDamping = true; 
-/* controls.minDistance = 5;
-controls.maxDistance = 20;
-controls.minPolarAngle = 0.5;
-controls.maxPolarAngle = 1.5; */
-/* controls.autoRotate = false; */
-/* controls.target = new THREE.vector3(0, 1, 0);
- */controls.update();
-
+controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.update();
 
 renderer.outputEncoding = THREE.sRGBEncoding;
 
@@ -157,213 +160,39 @@ models.forEach(modelDetails => {
     modelContainer.add(scene);
     scene.scale.set(scale, scale, scale);
     scene.position.set(...position);
-  
-
   });
- 
 });
 
-
-
-/* loader.load( "/models/good_bush.glb", 
-    function ( gltf ) {
-        gltf.scene.position.set(0, -1.5, -2);
-        const model = gltf.scene.children[6];
-        model.children[0].material.metalness = 0;
-        model.children[1].material.metalness = 0;
-        scene.add(model);
-    }
-); */
-
-function animate(){
+function animate() {
   requestAnimationFrame(animate);
   
-	raycaster.setFromCamera( mouse, camera );
-	const intersects = raycaster.intersectObjects(modelContainer.children);
+  raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObjects(modelContainer.children);
   if (intersects.length > 0) {
     container.style.cursor = "pointer";
   } else {
-    container.style.cursor = "initial"; 
+    container.style.cursor = "initial";
   }
   
   modelContainer.children.forEach(child => {
     child.rotation.y += 0.007;
   });
 
-  controls.update();  
+  controls.update();
   renderer.render(scene, camera);
 }
 
-function onMouseClick() {
-
-  
-  raycaster.setFromCamera( mouse, camera );
-	const intersects = raycaster.intersectObjects(modelContainer.children);
-  if (intersects.length > 0) {
-    const { link } = intersects[0].object.userData;
-    location.replace (link, '_blank');
-  }
-var clientClickX, clientClickY;
-
-  renderer.domElement.addEventListener('pointerdown', function(ev){
-      clientClickX = ev.clientX;
-      clientClickY = ev.clientY;
-  }, false);
-
-  renderer.domElement.addEventListener('pointerup', function (ev){
-      if (ev.target == renderer.domElement) {
-          var x = ev.clientX;
-          var y = ev.clientY;
-          // If the mouse moved since the mousedown then don't consider this a selection
-          if( x != clientClickX || y != clientClickY )
-              return;
-
-          var v = new THREE.Vector3((x/window.innerWidth)*2-1, -(y/window.innerHeight)*2+1, 0.5);
-          projector.unprojectVector(v, camera);
-          var ray = new THREE.Ray(camera.position, 
-                          v.subSelf(camera.position).normalize());
-          var intersects = ray.intersectObjects(SOLARSIM.OrbitalObjectsMeshes());
-          if (intersects.length > 0) {
-              SOLARSIM.GUI.ShowObjectInfo( intersects[0].object.name );
-          } else {
-              // Nothing Clicked so set the currently selected object to nothing
-              SOLARSIM.CurrentlySelectedPlanet = null;
-              SOLARSIM.GUI.HideObjectInfo();
-          }
-      }
-  }, false);
-
-}
-
-/* const clickCoords = new THREE.Vector2();
-
-controls.addEventListener('start', (e) => {
-        clickCoords.x = e.target.target.x;
-	clickCoords.y = e.target.target.y;
-});
-
-controls.addEventListener('end', (e) => {
-	if (clickCoords.x == e.target.target.x && clickCoords.y == e.target.target.y) return onClickHandler(e);
-});
-
-const onClickHandler = (event) => {
-        //Do someting
-} */
-
-
-
-
-
-function onWindowResize () {
+function onWindowResize() {
   camera.aspect = container.clientWidth / container.clientHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(container.clientWidth, container.clientHeight);
 }
 
+// Event listeners
 window.addEventListener('resize', onWindowResize);
+window.addEventListener('mousedown', onMouseDown, false);
 window.addEventListener('mousemove', onMouseMove, false);
-
-window.addEventListener('click', onMouseClick, false);
+window.addEventListener('mouseup', onMouseUp, false);
 
 animate();
 onWindowResize();
-
-
-
-
-/* function setup() {
- 
-  setupText();
-}
-
-function setupText() {
-  let loader = new THREE.FontLoader();
-  
-  loader.load("https://s3-us-west-2.amazonaws.com/s.cdpn.io/254249/helvetiker_regular.typeface.json", function (font) {
-    let message = "#CodePenOrebro";
-    let geometry = new THREE.TextGeometry(message, {
-      font: font,
-      size: 500,
-      height: 5,
-      curveSegments: 12,
-      bevelEnabled: true,
-      bevelThickness: 10,
-      bevelSize: 3,
-      bevelSegments: 5
-    });
-    let material = new THREE.MeshPhongMaterial({ 
-      color: 0xff00ff, 
-      overdraw: 0.5,
-      shininess: 70
-    });
-    geometry.center();
-    let mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-  });
-} */
-/* 
-const loader = new THREE.FontLoader();
-const textMesh = new THREE.Mesh();
-const createTypo = font => {
-  const word = "floating";
-  const typoProperties = {
-    font: font,
-    size: cubeSize,
-    height: cubeSize / 2,
-    curveSegments: 12,
-    bevelEnabled: true,
-    bevelThickness: 10,
-    bevelSize: 6,
-    bevelOffset: 1,
-    bevelSegments: 8
-  };
-  const text = new THREE.TextGeometry(word, typoProperties);
-  textMesh.geometry = text;
-  textMesh.material = material;
-  textMesh.position.x = cubeSize * -2;
-  textMesh.position.z = cubeSize * -1;
-  scene.add(textMesh);
-};
-loader.load(
-  "https://threejs.org/examples/fonts/helvetiker_regular.typeface.json",
-  createTypo
-);
-
-function loadFont() {
-  var loader = new THREE.FontLoader();
-  loader.load(
-    "https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/fonts/gentilis_regular.typeface.json",
-    function(res) {
-      createText(res);
-    }
-  ); */
-
-/*   var render = function() {
-    requestAnimationFrame(render);
-    animate();
-  };
-  loadFont();
-  
-  render();
-function createText(font) {
-  textGeo = new THREE.TextGeometry("Iz", {
-    font: font,
-    size: 40,
-    height: 5,
-    curveSegments: 10,
-    bevelEnabled: true,
-    bevelThickness: 1,
-    bevelSize: 1.8,
-    bevelOffset: 0,
-    bevelSegments: 5,
-    bevelEnabled: true
-  });
-  textGeo.computeBoundingBox();
-  textGeo.computeVertexNormals();
-  text = new THREE.Mesh(textGeo, cubeMat);
-  text.position.x = -textGeo.boundingBox.max.x / 2;
-  text.castShadow = true;
-  scene.add(text);
-}
- */
-
